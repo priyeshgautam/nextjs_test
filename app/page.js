e115f0124cb96b360822d0185535db38cd6d7d3b
priyesh <priyeshgautam8@gmail.com>
2026-03-22 15:06:27 +0530

    note component

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
+import NoteClient from "@/components/NoteClient"
 import dbConnection from "@/lib/database";
 import Image from "next/image";
 
 export default async function Home() {
   await dbConnection();
   return (<div>
-    <p> Notes app</p>
+    <p> Notes App</p>
+    <NoteClient></NoteClient>
   </div>);
 }
---
+++ components/NoteClient.jsx
@@ -0,0 +1,10 @@
+"use client"
+import React from 'react'
+
+function NoteClient() {
+  return (
+    <div>Note Client</div>
+  )
+}
+
+export default NoteClient
